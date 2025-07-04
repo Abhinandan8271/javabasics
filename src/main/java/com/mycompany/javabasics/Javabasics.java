03dce96bbca7f8d4366870b29513258df93fb93f
Abhinandan Bhardwaj <abhinandanbhardwaj04@gmail.com>
2025-07-04 09:32:12 +0530

    Initial commit

---
+++ src/main/java/com/mycompany/javabasics/Javabasics.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javabasics;
+
+/**
+ *
+ * @author abhin
+ */
+public class Javabasics {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        SingletonClass single=SingletonClass.getInstance();
+    }
+}
