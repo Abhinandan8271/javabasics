721071391ac4cda8fb0018029c90c14881222c54
Abhinandan Bhardwaj <abhinandanbhardwaj04@gmail.com>
2025-07-06 22:29:39 +0530

    commit

--- src/main/java/com/mycompany/javabasics/Javabasics.java
+++ src/main/java/com/mycompany/javabasics/Javabasics.java
@@ -13,5 +13,7 @@ public class Javabasics {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         SingletonClass single=SingletonClass.getInstance();
+        FormScreen screen=new FormScreen(0);
+        screen.setVisible(true);
     }
 }
